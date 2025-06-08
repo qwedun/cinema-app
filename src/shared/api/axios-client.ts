c2c92dfca4126a1cea454dde2c96688d4a9cdd85
Danil Sharifullin <qweber2019@mail.ru>
2025-06-08 22:06:26 +0300

    Changed api key

--- src/shared/api/axios-client.ts
+++ src/shared/api/axios-client.ts
@@ -3,7 +3,7 @@ import axios, { CreateAxiosDefaults } from 'axios';
 const axiosConfig: CreateAxiosDefaults = {
     baseURL: 'https://api.kinopoisk.dev/v1.4',
     headers: {
-        'X-API-KEY': '00FE019-60D4RWK-H1P5CK1-5SR3BWDT', //отказался от process.env, т.к не разобрался с конфигурацией .env файлов на Vercel
+        'X-API-KEY': '00FE019-60D4RWK-H1P5CK1-5SR3BWD', //отказался от process.env, т.к не разобрался с конфигурацией .env файлов на Vercel
         'Content-Type': 'application/json'
     },
     paramsSerializer: {
