ccae67756b0c4cc5f6e40b70b41a942dbbbbbe5e
qwedun <113780213+qwedun@users.noreply.github.com>
2025-09-19 16:01:14 +0300

    Update axios-client.ts

--- src/shared/api/axios-client.ts
+++ src/shared/api/axios-client.ts
@@ -3,7 +3,7 @@ import axios, { CreateAxiosDefaults } from 'axios';
 const axiosConfig: CreateAxiosDefaults = {
     baseURL: 'https://api.kinopoisk.dev/v1.4',
     headers: {
-        'X-API-KEY': 'M66TQTM-GH445ZH-QQEDYSW-79KW4TT', //отказался от process.env, т.к не разобрался с конфигурацией .env файлов на Vercel
+        'X-API-KEY': '3BD3C6M-W9XMB8G-G3RTXPN-1NPY31H', //отказался от process.env, т.к не разобрался с конфигурацией .env файлов на Vercel
         'Content-Type': 'application/json'
     },
     paramsSerializer: {
